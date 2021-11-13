064b21a6214c67b523505938238314baf5f20000
Christine Rowland <fordo29@gmail.com>
2021-11-13 16:23:11 -0700

    Add updated sidebars

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var game = new Game();
 var chooseClassicBtn = document.querySelector(".classic-mode");
 var chooseSpicyBtn = document.querySelector(".difficult-mode");
+var pirateSide = document.querySelector(".computer-side")
+var surferSide = document.querySelector(".human-side")
 var fighterBtn = document.querySelector(".fight");
 var turtleBtn = document.querySelector(".turtle");
 var mermaidBtn = document.querySelector(".mermaid");
@@ -19,17 +21,25 @@ fighterBtn.addEventListener("click", function(event) {
  });
 
 
-function displayPlayerInfo() {
-   surferPlayer.innerHTML = `
-   <img class="sidebar-color" src="./assets/surfer.png" alt="surfer">
-   <p class="sidebar-color">${game.surfer.name}</p>
-   <p class="sidebar-color" id="score">Wins: ${surfer.wins}</p>`
-}
 
 
 function surferPick(event) {
     game.surfer.choice = event.target.id;
     game.determineWinner();
+    displayPlayerInfo();
+}
+
+function displayPlayerInfo() {
+  surferSide.innerHTML = `
+    <img class="sidebar-color" src="./assets/surfer.png" alt="surfer">
+    <p class="sidebar-color">${game.surfer.name}</p>
+    <p class="sidebar-color" id="score">Wins: ${game.surfer.wins}</p><button type="button" class="change-game-button" name="change-game">Change Game?</button>`
+    ;
+
+  pirateSide.innerHTML = `
+    <img class="sidebar-color" src="./assets/pirate.png" alt="pirate">
+    <p class="sidebar-color">${game.pirate.name}</p>
+    <p class="sidebar-color" id="score">Wins: ${game.pirate.wins}</p>`;
 }
 
 function goHome() {
